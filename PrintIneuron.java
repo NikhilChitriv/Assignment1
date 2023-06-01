286a684f098a7a17722d49a2bbaa69407ff68d60
NikhilChitriv <130826061+NikhilChitriv@users.noreply.github.com>
2023-06-01 23:02:16 +0530

    Add files via upload

---
+++ PrintIneuron.java
@@ -0,0 +1,104 @@
+public class Main{
+  public static void main(String[] args) {
+    
+    int n=15;
+    for(int i=0;i<n;i++)
+    {
+     for(int j=0;j<n;j++)
+     {
+       if(i==0&&j<=(3*n)/4||j==(n-1)/3||i==n-1&&j<=(3*n)/4)
+       {
+       System.out.print("*");
+       }
+       else
+       {
+       System.out.print(" ");
+       }
+     }
+     
+     
+     
+     for(int j=0;j<n;j++)
+     {
+       if(i==j&&i<=(3*n)/4&&j<=(3*n)/4||j==0||j==(3*n)/4)
+       {
+       System.out.print("*");
+       }
+       else
+       {
+       System.out.print(" ");
+       }
+     }
+     
+     for(int j=0;j<n;j++)
+     {
+       if(j==0||i==0&&j<=(3*n)/4||i==(n-1)/2&&j<(3*n)/6||i==n-1&&j<=(3*n)/4)
+       {
+       System.out.print("*");
+       }
+       else
+       {
+       System.out.print(" ");
+       }
+     }
+     
+     
+     
+     for(int j=0;j<n;j++)
+     {
+       if(j==0 && i<n-1||i==n-1&&j<(3*n)/4&&j>0||j==(3*n)/4&&i<n-1)
+       {
+       System.out.print("*");
+       }
+       else
+       {
+       System.out.print(" ");
+       }
+     }
+     
+      for(int j=0;j<n;j++)
+     {
+       if(i==0&&j<(2*n)/4||j==0||j==(2*n)/4&&i<(n-1)/2&&i>0
+       ||i==(n-1)/2&&j<(2*n)/4||i-j==(n)/2)
+       
+       
+       
+       {
+       System.out.print("*");
+       }
+       else
+       {
+       System.out.print(" ");
+       }
+     }
+     
+     for(int j=0;j<n;j++)
+     {
+       if((j==0&&i>0&&i<n-1||i==0&&j<(3*n)/4&&j>0||j==(3*n)/4&&i>0&&i<n-1||
+           i==n-1&&j<(3*n)/4&&j>0))
+             
+       {
+       System.out.print("*");
+       }
+       else
+       {
+       System.out.print(" ");
+       }
+     }
+     
+     
+      for(int j=0;j<n;j++)
+     {
+        if(i==j&&i<=(3*n)/4&&j<=(3*n)/4||j==0||j==(3*n)/4)
+       {
+       System.out.print("*");
+       }
+       else
+       {
+       System.out.print(" ");
+       }
+     }
+      System.out.println();
+    }
+  }
+}
